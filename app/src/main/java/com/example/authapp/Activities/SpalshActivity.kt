8103aeb0a85a27b311e6ee73b623f6fcc68d3c57
Udishan <ishanka.udishan@pearson.com>
2021-05-02 07:20:22 +0530

    splash activity added

--- app/src/main/java/com/example/authapp/Activities/SpalshActivity.kt
+++ app/src/main/java/com/example/authapp/Activities/SpalshActivity.kt
@@ -1,14 +1,12 @@
-@file:Suppress("PackageName")
-
 package com.example.authapp.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.authapp.R
 
-class MainActivity : AppCompatActivity() {
+class SpalshActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_spalsh_screen)
     }
 }
